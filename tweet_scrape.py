0b5c45bf712a6f590d37f73a6216027f14c327a5
Will Badart <wbadart@nd.edu>
2018-03-27 16:03:35 -0400

    Modified tweet_scrape.py - Made corrections to changes in @f9034a5 - Sends twitter credentials to program through command line args - Modified file saving behavior (writes tweets in batches now)
    Use of PersistentDict made it redundant to also have MyStreamListener be
    a context manager, so __enter__ and __exit__ have been removed.

--- tweet_scrape.py
+++ tweet_scrape.py
@@ -1,10 +1,14 @@
-import json
-import smtplib
-import time
+#!/usr/bin/env python3
+
+'''
+tweet_scrape.py
+
+Stream crypto-related tweets into a file via tweepy.
+created: MAR 2018
+'''
 
 import tweepy
-
-from os import environ
+from wbutil import PersistentDict
 
 TERMS = [
     "bitcoin", "BTC",
@@ -18,29 +22,28 @@ TERMS = [
 
 class MyStreamListener(tweepy.StreamListener):
 
-    def __init__(path):
-        self.path = path
-        self.file_out = None
-
-    def __enter__(self):
-        self.file_out = open(path, 'a')
-        return self
-
-    def __exit__(self, *args):
-        self.file_out.close()
+    def __init__(self, path, api, batch_size=100):
+        self.data = PersistentDict({'tweets': []}, path=path)
+        self.api = api
+        self.batch_size = batch_size
 
     def on_status(self, status):
+        print(('Got tweet %d' % len(self.data['tweets'])), '\r', end='')
         try:
             tweet = {
-                "id" : status.id_str,
-                "text" : status.text,
-                "hashtags" : status.entities.get('hashtags'),
-                "retweets" : status.retweet_count,
-                "user_id" : status.user.id_str,
-                "time" : str(status.created_at)
+                "id": status.id_str,
+                "text": status.text,
+                "hashtags": status.entities.get('hashtags'),
+                "retweets": status.retweet_count,
+                "user_id": status.user.id_str,
+                "time": str(status.created_at)
             }
-            file_out.write(json.dumps(tweet) + "\n")
-        except:
+            self.data['tweets'].append(tweet)
+            if len(self.data['tweets']) % self.batch_size == 0:
+                print('Writing batch. Saving %d tweets...'
+                      % (len(self.data['tweets'])))
+                self.data.save()
+        except Exception:
             pass
 
     def on_error(self, status_code):
@@ -48,14 +51,25 @@ class MyStreamListener(tweepy.StreamListener):
 
 
 def main():
-    from sys import argv
-    auth = tweepy.OAuthHandler(environ.get('TWEEPY_HANDLE'))
-    auth.set_access_token(environ.get('TWEEPY_TOKEN'))
+    from argparse import ArgumentParser
+    parser = ArgumentParser(description='Stream tweets into a file.')
+    parser.add_argument('-o', '--output', help='destination for json data')
+    parser.add_argument('--consumerkey')
+    parser.add_argument('--consumersecret')
+    parser.add_argument('--accesskey')
+    parser.add_argument('--accesssecret')
+    args = parser.parse_args()
+
+    auth = tweepy.OAuthHandler(consumer_key=args.consumerkey,
+                               consumer_secret=args.consumersecret)
+    auth.set_access_token(key=args.accesskey, secret=args.accesssecret)
     api = tweepy.API(auth)
 
-    with MyStreamListener(argv[1]) as streamlistener:
-        stream = tweepy.Stream(auth=api.auth, listener=streamlistener)
+    streamlistener = MyStreamListener(args.output, api)
+    stream = tweepy.Stream(auth=api.auth, listener=streamlistener)
+    try:
         stream.filter(track=TERMS)
+    finally:
         stream.disconnect()
 
 
