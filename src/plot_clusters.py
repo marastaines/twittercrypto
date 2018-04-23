8e989f743e5753d19ae671a78cc13229519d3dac
Will Badart <wbadart@live.com>
2018-04-22 20:32:17 -0400

    Multiple plots for algorithm comparison

--- src/plot_clusters.py
+++ src/plot_clusters.py
@@ -12,30 +12,51 @@ created: APR 2018
 
 import matplotlib.pyplot as plt
 from fileinput import input as fileinput
-from sklearn.decomposition import TruncatedSVD
 from wbutil import lmap
+
+from nltk.tokenize import TweetTokenizer
+from sklearn.cluster import KMeans, SpectralClustering
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.preprocessing import scale
+from sklearn.svm import OneClassSVM
 
 from cluster2 import load_tweets, mkpipeline, write_clusters
 
-COLORS = 'rb'
+COLORS = 'rbgk'
+N_CLUSTERS = 3
 
 
 def main():
     with fileinput() as fs:
         tweets = list(load_tweets(fs))
 
-    pipeline = mkpipeline(tweets)
-    t_vectors = pipeline.named_steps.vectorize.transform(tweets)
-    svd = TruncatedSVD().fit_transform(t_vectors)
-    clustering = pipeline.predict(tweets)
+    tvecs = CountVectorizer().fit_transform(tweets)
+    svd = TruncatedSVD().fit_transform(tvecs)
 
-    plt.title(f'Tweet Clusters w/ SVD to 2-dimensional '
-              f'space and {len(tweets)} tweets')
+    fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
+    fig.title('{}-Clustering Strategies for {} Tweets'.format(
+        N_CLUSTERS, len(tweets)))
 
-    plt.scatter(svd[:,0], svd[:,1], c=lmap(COLORS.__getitem__, clustering))
+    ax1.set_title('Spectral Clustering on SVD')
+    spect = SpectralClustering(n_clusters=N_CLUSTERS).fit_predict(svd)
+    ax1.scatter(svd[:,0], svd[:,1], c=lmap(COLORS.__getitem__, spect))
+
+    ax2.set_title('KMeans on SVD')
+    kmean = KMeans(n_clusters=N_CLUSTERS).fit_predict(svd)
+    ax2.scatter(svd[:,0], svd[:,1], c=lmap(COLORS.__getitem__, kmean))
+
+    ax3.set_title('Spectral Clustering on Tweet Vectors')
+    spect1 = SpectralClustering(n_clusters=N_CLUSTERS).fit_predict(tvecs)
+    ax3.scatter(svd[:,0], svd[:,1], c=lmap(COLORS.__getitem__, spect1))
+
+    ax4.set_title('KMeans on Tweet Vectors')
+    kmean1 = KMeans(n_clusters=N_CLUSTERS).fit_predict(tvecs)
+    ax4.scatter(svd[:,0], svd[:,1], c=lmap(COLORS.__getitem__, kmean1))
+
     plt.show()
 
-    write_clusters(clustering, tweets)
+    # write_clusters(clustering, tweets, n_clusters=N_CLUSTERS)
 
 
 if __name__ == '__main__':
