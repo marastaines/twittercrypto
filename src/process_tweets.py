acc38d3668c2985371f786aba59c74c960133134
marastaines <marastaines@gmail.com>
2018-05-03 21:26:36 -0400

    Process_tweets is now way more coherent (and commented!)

--- src/process_tweets.py
+++ src/process_tweets.py
@@ -1,23 +1,30 @@
 #!usr/bin/python
 
 ## Shane Ryan + Mara
-"""This is a big old frankensteined disaster that I built off of Shane's code.
-    I know its really gross and confusing, its first thing up to fix.
-    Just needed it done to get any sort of regression going.
+"""
+Usage:
+$python3 process_tweets.py <tweet-json-file>
 
-    It uses price_scrape.py as a module
-    CHANGE FILE PATH ON LINE 118 to correct tweet file
+Outputs to:
+instances.txt
 
-    Note: (1) Tweet chunking is done so that the whole file isn't stored in RAM.
-          (2) The repeated calls to import_tweets_by_block don't start reading from the beginning of the file for each call, they jump to where the last call left off
-    These should make this script efficient enough to not destroy your computer even with a massive tweet file"""
+
+This program takes a text file of tweet json objects as input.
+It uses a start time, end time, and size of time division (eg 1 hour) to create blocks of tweets
+Each block of tweets is used to generate 1 instance with 4 features.
+Import_tweets_by_block is weird due to the size of the text files. 
+Any data structure holding the entire text file in RAM would be a significant resource drain.
+This solution allows the file to be opened, read from, and closed. The returned tweets are processed into an instance, then the file is opened for the next chunk
+To avoid reading from the start of the file every time, the end read location is returned by the function.
+When this location is passed to the next function call, it jumps to that location in the file.
+"""
 
 import json
 import re
 from textblob import TextBlob
 import datetime as dt
 import numpy as np
-import price_scrape as ps
+import sys
 
 ## read tweets in from source file in easily accessible object format
 class Tweet(object):
@@ -40,13 +47,6 @@ class Tweet(object):
 
     def get_tweet_sentiment(self):
         analysis = TextBlob(self.text)
-        # sentiment
-        """if analysis.sentiment.polarity > 0.5:
-            return analysis.sentiment.polarity
-        elif analysis.sentiment.polarity < -0.5:
-            return analysis.sentiment.polarity
-        else:
-            return 0"""
         return analysis.sentiment.polarity
 
 
@@ -72,17 +72,24 @@ def import_tweets(filename):
                 raw_tweets.append(json.loads(line))
 
     return raw_tweets
+
 def import_tweets_by_block(filename, s, e, start_loc):
 
+    #Store raw tweet jsons where s (start time) < tweet time < e (end time)
     raw_tweets = list()
+    #Return the location in the file after the last tweet, so next call of function can jump there in file
     end_loc = 0
     with open(filename) as source:
+        #Jump to where last call left off
         source.seek(start_loc)
+
+        #The scraper occasionally didn't put a newline between two json dumps. This looks for that ("}{"), and handles it
         prob_string = "}{\"id\""
         curr_tweets = []
         inrange = 1
         line = source.readline()
         print(line)
+        #While there are lines to read and the end-datetime flag hasn't been switched
         while(line and inrange):
             prob = line.find(prob_string)
             if prob != -1:
@@ -105,41 +112,46 @@ def import_tweets_by_block(filename, s, e, start_loc):
         end_loc = source.tell()
     return raw_tweets, end_loc
 
-delta = dt.timedelta(seconds = 3600)
-start = dt.datetime(2018, 4, 6, hour=0, minute=0, second=0)
-end = start + delta
-end_final = dt.datetime(2018, 4, 7, hour=0, minute=0, second=0)
-start_loc = 0
+if __name__ == '__main__':
+    filename = sys.argv[1]
+    delta = dt.timedelta(seconds = 3600)                                #Size of time chunk for each instance
+    start = dt.datetime(2018, 4, 7, hour=0, minute=0, second=0)         #Start of first chunk
+    end = start + delta                                                 #End of first chunk
+    end_final = dt.datetime(2018, 4, 14, hour=0, minute=0, second=0)    #End of entire range
+    start_loc = 0
 
-out = open("instances.txt", "w+")
-while end_final >= end:
-    print(str(end))
-    print(end.timestamp())
-    raw_tweets, start_loc = import_tweets_by_block("/home/mara/Documents/2018sp/ml/appa.out.txt", start, end, start_loc)
-    start = end
-    end += delta
-    #raw_tweets = import_tweets_by_block("mini.txt", "2018-03-28 17:22:08", "2018-03-28 17:22:10")
+    out = open("instances.txt", "w+")
+    while end_final >= end:
+        print(str(end))
+        print(end.timestamp())
+        #Get raw tweets for 1 time chunk, save location reached in file
+        raw_tweets, start_loc = import_tweets_by_block(filename, start, end, start_loc)
+        
+        #Create list of Tweet objects
+        tweet_data = []
+        for tweet in raw_tweets:
+            tweet_data.append(process_tweet(tweet))
 
-    tweet_data = []
-    for tweet in raw_tweets:
-        tweet_data.append(process_tweet(tweet))
+        #Iterate over tweet objects to calculate features
+        sentiment_block_score = 0           #Average sentiment in block
+        retweet_block_score = 0             #Average # Retweets in block
+        sentiment_sd = 0                    #Sentiment standard deviation in block
+        sent = []
+        for n, tweet in enumerate(tweet_data):
+            sentiment_block_score += tweet.sentiment
+            sent.append(tweet.sentiment)
+            retweet_block_score += tweet.retweets
+            
+        #If this time chunk has >0 tweets, finish calculations
+        if len(tweet_data) != 0:
+            sentiment_block_score /= len(tweet_data)
+            sent = np.array(sent)
+            sentiment_sd = np.std(sent)
+            retweet_block_score /= len(tweet_data)
 
-    sentiment_block_score = 0
-    retweet_block_score = 0
-    sentiment_sd = 0
-    sent = []
-    for n, tweet in enumerate(tweet_data):
-        sentiment_block_score += tweet.sentiment
-        sent.append(tweet.sentiment)
-        retweet_block_score += tweet.retweets
-        """print("Tweet %d\n\nContent:\n%s\n\nSentiment:\t%d\n\nTime:\t%s\n\n"
-                % (n+1, tweet.text, tweet.sentiment, tweet.time))"""
-    if len(tweet_data) != 0:
-        sentiment_block_score /= len(tweet_data)
-        sent = np.array(sent)
-        sentiment_sd = np.std(sent)
-        retweet_block_score /= len(tweet_data)
-    price = ps.get_historical("BTC", end.timestamp())
-    out.write(str(start) + "," + str(end) + "," + str(len(tweet_data)) + "," +str(sentiment_block_score) + "," + str(sentiment_sd) + "," + str(retweet_block_score) + "," + str(price) + "\n")
-out.close()
+        #Write time chunk, features to file
+        out.write(str(start) + "," + str(end) + "," + str(len(tweet_data)) + "," +str(sentiment_block_score) + "," + str(sentiment_sd) + "," + str(retweet_block_score) +"\n")
+        start = end
+        end += delta
+    out.close()
 
